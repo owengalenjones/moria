d94a59175ea7e153037ed1849c95068fdcd14def
Owen Jones <owengalenjones@gmail.com>
2016-06-14 22:58:35 -0400

    final cleanup

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject moria "0.1.0-SNAPSHOT"
+(defproject moria "0.1.0"
   :description "CLJS Wrapper for Mithril.js"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/owengalenjones/moria"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [cljsjs/mithril "0.2.4-0"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.1"]]
-  :npm {:dependencies [[source-map-support "0.4.0"]]}
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
   :target-path "target")
--- scripts/watch.clj
+++
@@ -1,6 +0,0 @@
-(require '[cljs.build.api :as b])
-
-(b/watch "src"
-  {:main 'moria.core
-   :output-to "out/moria.js"
-   :output-dir "out"})
