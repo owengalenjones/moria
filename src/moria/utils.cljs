ef68fd2662f32b396fe5a5525e1fae69aeb1a786
Owen Galen Jones <owengalenjones@gmail.com>
2016-06-14 23:10:35 -0400

    Update utils.cljs

--- src/moria/utils.cljs
+++ src/moria/utils.cljs
@@ -1,6 +1,8 @@
 (ns moria.utils
   (:require cljsjs.mithril))
 
+;TODO: this can just be clojure.spec now which will be excellent
+
 (defn valid-component?
   "Checks a component object for a mandatory :view key.
   For nicer error messages."
