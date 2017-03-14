fb97fc2fc680b789c33a8f2e33ad8371c1c0e448
Owen Jones <owengalenjones@gmail.com>
2017-03-13 21:46:42 -0400

    bump version & mithril dep

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject moria "0.1.0"
+(defproject moria "1.0.1-0"
   :description "CLJS Wrapper for Mithril.js"
   :url "https://github.com/owengalenjones/moria"
   :signing {:gpg-key "owengalenjones@gmail.com"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
-                 [cljsjs/mithril "0.2.4-0"]]
+                 [cljsjs/mithril "1.0.1-0"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.1"]]
   :source-paths ["src" "target/classes"]
